edb7964999bc27537c984da80963e12ff2a8916c
august-ehrlich <74673841+august-ehrlich@users.noreply.github.com>
2021-03-22 00:34:35 -0500

    Update spotify_mp3.py

--- spotify_mp3.py
+++ spotify_mp3.py
@@ -23,27 +23,27 @@ def spotify_txt():
     # favorite songs list
     if toggle == 1:
         n = 0
-        title = {'name': 'Liked Songs'}
+        title = {'name': 'Liked Songs'} # sets playlist title to "Liked Songs"
         while n > -1:
-            results = sp.current_user_saved_tracks(limit=50, offset=50*n)
+            results = sp.current_user_saved_tracks(limit=50, offset=50*n) # gets the list of tracks from spotify 50 at a time (maximum allowed by the spotify API)
             for idx, item in enumerate(results['items']):
-                track = item['track']
+                track = item['track'] # saves the information about the track to search_title and other_info arrays
                 search_title.append(track['artists'][0]['name'] + " – " + track['name'] + ' audio')
                 other_info.append({'name': track['name'], 'artist': track['artists'][0]['name'],
                                    'album': track['album']['name'], 'track_number': track['track_number'],
                                    'image_location': track['album']['images'][0]['url']})
             n += 1
-            if len(results['items']) < 50:
+            if len(results['items']) < 50: # stops sending requests to the API once a set of songs less than 50 has been recieved, meaning the end of the playlist has been reached
                 break
     # playlist w/ identifier code
     elif toggle == 2:
         n = 0
         uri = input("Please input the playlist identifier code (i.e. 78pJV5u1zBe6vCrbNcx7Ic)\n")
-        title = sp.playlist(uri, fields='name')
+        title = sp.playlist(uri, fields='name') # sets the playlist name to whatever name it has on spotify
         while n > -1:
             results = sp.playlist_items(playlist_id=uri,
                                         fields='items(track(name,artists(name),album(name,images),track_number))',
-                                        limit=100, offset=100*n)
+                                        limit=100, offset=100*n) # gets the information shown by "fields" 100 songs at a time
             for idx, item in enumerate(results['items']):
                 track = item['track']
                 search_title.append(track['artists'][0]['name'] + ' ' + track['name'] + ' audio')
@@ -54,9 +54,10 @@ def spotify_txt():
                 break
             n += 1
     # album w/ identifier code
+    # functionally almost identical to the playlist finder, just taking an album URI instead of a playlist URI
     elif toggle == 3:
         uri = input("Please input the album identifier code (i.e. 1kwAv74rVTTGMpawGsXtiE)\n")
-        title = sp.album(uri)
+        title = sp.album(uri) 
         results = sp.album_tracks(album_id=uri, limit=50)
         for idx, item in enumerate(results['items']):
             search_title.append(item['artists'][0]['name'] + ' ' + item['name'] + ' audio')
@@ -70,12 +71,12 @@ def youtube_search(search_list):
     url_list = []
     x = 1
     for terms in search_list:
-        try:
+        try: # uses the web scraper version of Youtube search to try and find each of the things in the search list, which is just the search_title array from spotify_txt
             results = YoutubeSearch(terms.lower(), max_results=1).to_dict()
             print(str(x) + ' ' + results[0]['title'])
             url_list.append("https://www.youtube.com" + results[0]['url_suffix'])
             x += 1
-        except:
+        except: # if the web scraper version fails, as it sometimes does, it then goes to the official Google Youtube API (however, this version has a limit to the number of requests you can send in a day, so it doesn't work for massive amounts of songs)
             api_service_name = "youtube"
             api_version = "v3"
             DEVELOPER_KEY = "AIzaSyCGo4DdnZwC9PgJLQyozcI7spNEkMwQYj8"
@@ -100,16 +101,16 @@ def youtube_download(song_list, playlist, urls):
     for links in urls:
         ydl_opts = {
             'format': 'mp4',
-            'outtmpl': "C:\\Users\\" + os.getlogin() + "\\Desktop\\" + playlist + "\\" + song_list[n] + '.mp4'
+            'outtmpl': "C:\\Users\\" + os.getlogin() + "\\Desktop\\" + playlist + "\\" + song_list[n] + '.mp4' # uses song information to create a unique filepath to download the .mp4 files to
         }
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([links])
-        filepaths.append("C:\\Users\\" + os.getlogin() + "\\Desktop\\" + playlist + "\\" + song_list[n] + '.mp4')
+            ydl.download([links]) # uses the youtube downloader to download each of the urls that were found using the youtube_search function as an .mp4 file
+        filepaths.append("C:\\Users\\" + os.getlogin() + "\\Desktop\\" + playlist + "\\" + song_list[n] + '.mp4') # attaches the file path to the .mp4 file to the array, so that future steps can find the file again
         n += 1
     return filepaths
 
 
-def music_convert(filepath):
+def music_convert(filepath): # this function just takes in a list of .mp4 files, opens them into the python program, finds the audio track, and saves the audio track as a .mp3 file with 256kbps bitrate
     for mp4_file in filepath:
         video_clip = VideoFileClip(mp4_file)
         audio_clip = video_clip.audio
@@ -120,7 +121,7 @@ def music_convert(filepath):
         os.remove(mp4_file)
 
 
-def album_art(url, folder, song_title):
+def album_art(url, folder, song_title): # this function downloads the album art, found using the spotify album art URLs, and saves the files to a temporary folder that is deleted at the end of the program
     file_name = "C:\\Users\\" + os.getlogin() + "\\Desktop\\" + folder + "\\album_art\\"
     song_title = song_title.replace('.mp3', '')
     if not os.path.isdir(file_name):
@@ -130,7 +131,7 @@ def album_art(url, folder, song_title):
     return file_name
 
 
-def add_mp3_tags(tags, collection):
+def add_mp3_tags(tags, collection): # this function adds MP3 tags to all of the songs, allowing Itunes or other music platforms to properly display cover art, song titles, album titles, artist names, and track numbers
 
     audiofile = eyed3.load('C:\\Users\\' + os.getlogin() + '\\Desktop\\' + collection + '\\' + tags['mp3_title'])
     if audiofile.tag is None:
@@ -151,26 +152,26 @@ mp3_tags = save[3]  # tags from spotify_txt
 songs = []  # holds sanitized file names
 
 iterate = 0  # iterator
-for media in save[0]:
+for media in save[0]: # cleans up the search_title array by removing any characters that Windows has trouble using in a filename
     for value in sanitize:
         media = media.replace(value, '')
     songs.append(media)
     mp3_tags[iterate].update({'mp3_title': media + '.mp3'})
     iterate += 1
 
-for value in sanitize:
+for value in sanitize: # cleans up the playlist title to do the same thing as above
     collection_name = collection_name.replace(value, '')
 
-files = youtube_download(songs, collection_name, youtube_search(songs))
+files = youtube_download(songs, collection_name, youtube_search(songs)) # downloads the files and then converts them to mp3 
 music_convert(files)
 
-iterate = 0  # iterator
+iterate = 0  # counts through the items in the mp3_tags array
 for items in mp3_tags:
     mp3_tags[iterate].update({'image_location': album_art(items['image_location'], collection_name,
-                                                          items['mp3_title'])})
+                                                          items['mp3_title'])}) # replaces the 'image_location' url with a filepath to the temporary cover art file
     iterate += 1
 
-for i in mp3_tags:
+for i in mp3_tags: # adds the mp3 tags to each item in the array
     add_mp3_tags(i, collection_name)
 
-shutil.rmtree('C:\\Users\\' + os.getlogin() + '\\Desktop\\' + collection_name + '\\album_art')
+shutil.rmtree('C:\\Users\\' + os.getlogin() + '\\Desktop\\' + collection_name + '\\album_art') # deletes the temporary cover art folder
